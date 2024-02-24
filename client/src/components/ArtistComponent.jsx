6e7d7b71ca8a1ad0f9f1691cb2775c1b401ef1ff
Juan Pablo Gonzalez <43384928+DrJP99@users.noreply.github.com>
2024-02-23 18:33:18 -0600

    better grid system

--- client/src/components/ArtistComponent.jsx
+++ client/src/components/ArtistComponent.jsx
@@ -2,14 +2,16 @@ import { Link } from 'react-router-dom';
 
 const ArtistComponent = ({ artist }) => {
 	return (
-		<div className="card">
-			<Link to={'/artist/' + artist.id}>
-				<div className="image-artist"></div>
-			</Link>
-			<div className="card-headers">
-				<p className="card-title text-center">
-					<Link to={'/artist/' + artist.id}>{artist.name}</Link>
-				</p>
+		<div className="card-container">
+			<div className="card">
+				<Link to={'/artist/' + artist.id}>
+					<div className="image-artist"></div>
+				</Link>
+				<div className="card-headers">
+					<p className="card-title text-center">
+						<Link to={'/artist/' + artist.id}>{artist.name}</Link>
+					</p>
+				</div>
 			</div>
 		</div>
 	);
